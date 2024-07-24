340fc48cd7245a5b012094237ce08615a271d3fa
Shreyan Dey <deyshreyan24@outlook.com>
2024-07-24 15:44:42 +0530

    Add loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,21 +24,24 @@ interface GamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
     apiClient
       .get<GamesResponse>("games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
-      });
+      })
+      .finally(() => setIsLoading(false));
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
